39b1f6ab5cc5d5a1851238f9480699e99b71a515
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-22 17:53:16 +0300

    Add dummy actions

--- app/src/main/java/com/gpetuhov/android/samplecoroutines3/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplecoroutines3/MainActivity.kt
@@ -2,11 +2,31 @@ package com.gpetuhov.android.samplecoroutines3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        load_button.setOnClickListener { loadUser() }
+        reset_button.setOnClickListener { resetUser() }
+    }
+
+    // === Private methods ===
+
+    private fun loadUser() {
+        // TODO
+        showUserName("Bob")
+    }
+
+    private fun resetUser() {
+        // TODO
+        showUserName("Hello World!")
+    }
+
+    private fun showUserName(name: String) {
+        user_name.text = name
     }
 }
