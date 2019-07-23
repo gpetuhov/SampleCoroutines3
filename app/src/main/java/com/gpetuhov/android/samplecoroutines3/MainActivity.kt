5e62e7c14be789759fb4c2e1525ff18ba728836b
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-23 11:26:32 +0300

    Add comments

--- app/src/main/java/com/gpetuhov/android/samplecoroutines3/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplecoroutines3/MainActivity.kt
@@ -11,6 +11,10 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,10 +27,29 @@ class MainActivity : AppCompatActivity() {
     // === Private methods ===
 
     private fun loadUserWithGlobalScope() {
+        // Here we use GlobalScope.
+        // This means that the coroutine will continue running (until completion or exception)
+        // as long as the application is running.
+        // If MainActivity is destroyed (for example, on screen rotation),
+        // coroutine will NOT be canceled, but results will not show on screen,
+        // because they will be delivered to the old activity, which is not on screen any more.
+        // This could lead to memory leaks.
+
+        // Despatchers.Main tells coroutine to run on main thread, because here we update UI
         GlobalScope.launch(Dispatchers.Main) {
-            val userOne = GlobalScope.async(Dispatchers.IO) { fetchUser() }
-            showUserName("Global Scope ${userOne.await()}") // back on UI thread
-            Timber.tag("MainActivity").d("Show user name")
+            Timber.tag(TAG).d("Start load user")
+
+            // Here we use Dispatchers.IO, so fetchUser is executed in the thread pool,
+            // specifically designed for blocking IO operations.
+            // This coroutine will not start immediately, because async is used.
+            // The coroutine will start when await is called.
+            val user = GlobalScope.async(Dispatchers.IO) { fetchUser() }
+
+            // Here we start fetchUser() on background thread. Results are returned to main thread.
+            showUserName("Global Scope ${user.await()}")
+
+            // This log statement will show even if MainActivity is destroyed
+            Timber.tag(TAG).d("Show user name")
         }
     }
 
@@ -36,7 +59,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetUser() {
-        // TODO
         showUserName("Hello World!")
     }
 
